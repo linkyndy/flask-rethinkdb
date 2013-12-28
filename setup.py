3d9563b9268b34060695a3e1c693ca6130a27798
linkyndy <linkyndy@gmail.com>
2013-12-28 10:51:59 +0200

    Updated version to 0.1
    Changed current version to 0.1 since 1.0 was too far fetched.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-RethinkDB',
-    version='1.0',
+    version='0.1',
     url='http://github.com/linkyndy/flask-rethinkdb',
     license='MIT',
     author='Andrei Horak',
