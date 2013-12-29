579157f1451f72ef618c8c3266a181aa498d01c8
linkyndy <linkyndy@gmail.com>
2013-12-29 15:29:48 +0200

    Bumped version to 0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-RethinkDB',
-    version='0.1',
+    version='0.2',
     url='http://github.com/linkyndy/flask-rethinkdb',
     license='MIT',
     author='Andrei Horak',
